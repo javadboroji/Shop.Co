dc8e7e1ce3092d6692b2c8bbe4d44723903b80b6
javd boroji <javadboroji22222@gmail.com>
2024-10-13 23:07:16 +0330

    fix:build Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        BASE_URL:"https://shop-d2aep052c-javads-projects-8d218047.vercel.ap"
-      },  
+     
 };
 
 export default nextConfig;
